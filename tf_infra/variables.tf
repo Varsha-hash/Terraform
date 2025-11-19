178c33ee5cda66a69f460c0e7baf4bce09286f24
Varsha-hash <varshapat28@gmail.com>
2025-11-19 17:36:56 +0530

    Updated jenkin file

--- tf_infra/variables.tf
+++ tf_infra/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
     description = "The AWS region to deploy the resouce"
-    type = "string"
+    type = string
     default = "ap-south-1"
   
 }
 
 variable "vpc_name" {
     description = "The name of the VPC"
-    type = "string"
+    type = string
 }
