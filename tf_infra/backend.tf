8ee4114bf17eb899d23a1aba2e70f3ef681654f6
Varsha-hash <varshapat28@gmail.com>
2025-11-19 17:16:11 +0530

    Updated jenkin file

--- tf_infra/backend.tf
+++ tf_infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket-varshapat"
-    key    = "env/${terraform_workspace}/terraform.state"
-    region = var.aws_region
+    # bucket = "terraform-state-bucket-varshapat"
+    # key    = "env/${terraform_workspace}/terraform.state"
+    # region = var.aws_region
    }
 
 }
